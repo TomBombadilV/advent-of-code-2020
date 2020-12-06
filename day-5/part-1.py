1bbf63bb3c7111a69dfd93b13d30c89bfb7c0c57
Carolyn <carolyngychan@gmail.com>
2020-12-05 22:36:32 -0800

    Day 5 Part 1

---
+++ day-5/part-1.py
@@ -0,0 +1,50 @@
+# Day 5 Part 1
+
+from typing import List
+
+def decode(seat: str, rows: int, cols: int) -> int:
+    """ Converts a seat into its seat code """
+    # Keep track of current seat window
+    row_left, row_right = 0, rows - 1
+    col_left, col_right = 0, cols - 1
+
+    # Iterate through seat string, updating current seat window
+    for c in seat:
+        if c == "F":  # Move window to front half
+            row_right = (row_left + row_right + 1) // 2
+        elif c == "B":  # Move window to back half
+            row_left = (row_left + row_right + 1) // 2
+        elif c == "L":  # Move window to left half
+            col_right = (col_left + col_right + 1) // 2
+        elif c == "R":  # Move window to right half
+            col_left = (col_left + col_right + 1) // 2
+        else:
+            print("Invalid letter")
+            return -1
+
+    # Convert to seast code
+    return row_left * 8 + col_left
+
+def get_max_code(seats: List[str], rows, cols) -> int:
+    """ Convert all seats into seat codes, find max """
+    max_code = 0
+    max_seat = ""
+    for seat in seats:
+        seat_code = decode(seat, rows, cols)
+        #max_code = max(max_code, seat_code)
+        if seat_code > max_code:
+            max_code = seat_code
+            max_seat = seat
+
+    return (max_code, max_seat)
+
+if __name__ == "__main__":
+    # Parse input
+    with open("input.txt", "r") as file:
+        lines = [line.strip() for line in file]
+
+    # Define plane size
+    rows, cols = 128, 8
+
+    # Get max seat code
+    print("Max seat code is {0}".format(get_max_code(lines, rows, cols)))
